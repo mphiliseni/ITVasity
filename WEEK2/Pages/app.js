aec054de65a664e6ee6e40116f571ac7d0fa1809
Mphiliseni <Mphilisenialexmhlongo@gmail.com>
2025-05-22 22:35:12 +0200

    Added ap.js

--- WEEK2/Pages/app.js
+++ WEEK2/Pages/app.js
@@ -1,19 +1,19 @@
 // add javascript logic to the app.js
-        document.addEventListener('DOMContentLoaded', function () {
-            alert('Welcome to CSS Week 2! 🎉 ');
-        });
+document.addEventListener('DOMContentLoaded', function () {
+    alert('Welcome to CSS Week 2! 🎉 ');
+});
 
-        const lazyImages = document.querySelectorAll('img.lazy');
+const lazyImages = document.querySelectorAll('img.lazy');
 
 const observer = new IntersectionObserver((entries, obs) => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      const img = entry.target;
-      img.src = img.dataset.src;
-      img.classList.remove('lazy');
-      obs.unobserve(img);
-    }
-  });
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            const img = entry.target;
+            img.src = img.dataset.src;
+            img.classList.remove('lazy');
+            obs.unobserve(img);
+        }
+    });
 });
 
 lazyImages.forEach(img => observer.observe(img));
